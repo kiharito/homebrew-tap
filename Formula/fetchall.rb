96e11ecbe2174b633e857eaf9d636ca406daab3d
kiharito <kihara13611.c7d7@gmail.com>
2023-10-31 22:15:29 +0900

    add desc/homepagge

---
+++ Formula/fetchall.rb
@@ -0,0 +1,7 @@
+class Fetchall < Formula
+  desc "Run `git fetch` on all registered directories at once."
+  homepage "https://github.com/kiharito/fetchall"
+  url ""
+  sha256 ""
+  license ""
+end
